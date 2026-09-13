322e487e6054c204a688a8bd79273cda8dca923b
Mohammed Nadeem Sufiyan <sufiyanmohammed098@gmail.com>
2026-09-13 22:58:17 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,45 @@
 locals {
+  region = var.aws_region
 
-  region          = "eu-west-1"
-  name            = "tws-eks-cluster"
-  vpc_cidr        = "10.0.0.0/16"
-  azs             = ["eu-west-1a", "eu-west-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+  name = "${var.project_name}-${var.environment}"
+
+  vpc_cidr = "10.0.0.0/16"
+
+  azs = [
+    "${var.aws_region}a",
+    "${var.aws_region}b",
+    "${var.aws_region}c"
+  ]
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.11.0/24",
+    "10.0.12.0/24",
+    "10.0.13.0/24"
+  ]
+
+  intra_subnets = [
+    "10.0.21.0/24",
+    "10.0.22.0/24",
+    "10.0.23.0/24"
+  ]
+
   tags = {
-    example = local.name
+    Project     = var.project_name
+    Environment = var.environment
+    Terraform   = "true"
   }
-
 }
 
 provider "aws" {
-
   region = local.region
 
+  default_tags {
+    tags = local.tags
+  }
 }
