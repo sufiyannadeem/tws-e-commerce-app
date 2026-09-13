fc00a4e87493f29d0c615f50a440c4a08fb2a33b
Mohammed Nadeem Sufiyan <sufiyanmohammed098@gmail.com>
2026-09-13 22:58:55 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,41 @@
 variable "aws_region" {
-  description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "eu-west-1"
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "e-commerce-app"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
 }
 
 variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  default     = "t2.medium"
+  description = "EC2 instance type for Jenkins"
+  type        = string
+  default     = "m7i-flex.large"
 }
 
-variable "my_enviroment" {
-  description = "Instance type for the EC2 instance"
-  default     = "dev"
+variable "ssh_cidr" {
+  description = "CIDR allowed to access SSH"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "jenkins_cidr" {
+  description = "CIDR allowed to access Jenkins on port 8080"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "public_key_path" {
+  description = "Path to the SSH public key"
+  type        = string
+  default     = "terra-key.pub"
 }
