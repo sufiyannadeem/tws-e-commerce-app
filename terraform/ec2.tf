ccecb66c2732d6b8ba4389bbea6f57bd462a591e
Mohammed Nadeem Sufiyan <sufiyanmohammed098@gmail.com>
2026-09-13 22:55:36 +0530

    Refactor EC2 and security group configurations
    Updated AWS resources for Jenkins and Bastion host, including security groups and instance configurations. Modified AMI data source and adjusted user data paths.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,47 +1,61 @@
-data "aws_ami" "os_image" {
-  owners = ["099720109477"]
+data "aws_ami" "ubuntu" {
   most_recent = true
+  owners      = ["099720109477"]
+
   filter {
     name   = "state"
     values = ["available"]
   }
+
   filter {
-    name = "name"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/*24.04-amd64*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "terra-automate-key"
-  public_key = file("terra-key.pub")
+  public_key = file("${path.module}/${var.public_key_path}")
+
+  tags = {
+    Name        = "terra-automate-key"
+    Environment = var.environment
+    Project     = var.project_name
+  }
 }
 
-resource "aws_default_vpc" "default" {
 
-}
 
-resource "aws_security_group" "allow_user_to_connect" {
-  name        = "allow TLS"
-  description = "Allow user to connect"
-  vpc_id      = aws_default_vpc.default.id
+resource "aws_security_group" "jenkins" {
+  name        = "${local.name}-jenkins-sg"
+  description = "Security group for Jenkins EC2"
+  vpc_id      = module.vpc.vpc_id
+
   ingress {
-    description = "port 22 allow"
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = " allow all outgoing traffic "
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.ssh_cidr]
   }
 
   ingress {
-    description = "port 80 allow"
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -49,7 +63,7 @@ resource "aws_security_group" "allow_user_to_connect" {
   }
 
   ingress {
-    description = "port 443 allow"
+    description = "HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
@@ -57,30 +71,117 @@ resource "aws_security_group" "allow_user_to_connect" {
   }
 
   ingress {
-    description = "port 8080 allow"
+    description = "Jenkins"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
+    cidr_blocks = [var.jenkins_cidr]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "mysecurity"
+    Name        = "${local.name}-jenkins-sg"
+    Environment = var.environment
+    Project     = var.project_name
   }
 }
 
-resource "aws_instance" "testinstance" {
-  ami             = data.aws_ami.os_image.id
-  instance_type   = var.instance_type 
-  key_name        = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.allow_user_to_connect.name]
-  user_data = file("${path.module}/install_tools.sh")
-  tags = {
-    Name = "Jenkins-Automate"
-  }
+resource "aws_instance" "jenkins" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.deployer.key_name
+  subnet_id                   = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.jenkins.id]
+  associate_public_ip_address = true
+
+  user_data                   = file("${path.module}/install_tools.sh")
+  user_data_replace_on_change = true
+
   root_block_device {
-    volume_size = 20
-    volume_type = "gp3"
+    volume_size           = 30
+    volume_type           = "gp3"
+    encrypted             = true
+    delete_on_termination = true
   }
-  
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  tags = {
+    Name        = "Jenkins-Automate"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+
+  depends_on = [
+    module.vpc
+  ]
+}
+
+resource "aws_security_group" "bastion" {
+  name        = "${local.name}-bastion-sg"
+  description = "Security group for Bastion Host"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH from trusted IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${local.name}-bastion-sg"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.deployer.key_name
+  subnet_id                   = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.bastion.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size           = 10
+    volume_type           = "gp3"
+    encrypted             = true
+    delete_on_termination = true
+  }
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  tags = {
+    Name        = "Bastion-Host"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+
+  depends_on = [
+    module.vpc
+  ]
 }
