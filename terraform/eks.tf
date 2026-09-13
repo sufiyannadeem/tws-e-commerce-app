f4f2ec1932569bcd4ca4b5ea660fa74e432e5c10
Mohammed Nadeem Sufiyan <sufiyanmohammed098@gmail.com>
2026-09-13 22:57:10 +0530

    Upgrade EKS module and modify cluster settings
    Updated EKS module version and configuration, including changes to node group settings and security group ingress rules.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,73 +1,103 @@
 module "eks" {
-
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "21.25.0"
 
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
+  name               = local.name
+  kubernetes_version = "1.34"
 
-  cluster_addons = {
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.private_subnets
+
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  access_entries = {
+    sufiyan = {
+      principal_arn = "arn:aws:iam::80539xxxxxx:user/sufiyan"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
+
+  addons = {
+    vpc-cni = {
+      most_recent    = true
+      before_compute = true
+    }
+
+    kube-proxy = {
+      most_recent    = true
+      before_compute = true
+    }
+
     coredns = {
       most_recent = true
     }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
+
+    eks-pod-identity-agent = {
       most_recent = true
     }
   }
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.public_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # EKS Managed Node Group(s)
-
-  eks_managed_node_group_defaults = {
-
-    instance_types = ["t2.large"]
-
-    attach_cluster_primary_security_group = true
-
-  }
-
 
   eks_managed_node_groups = {
-
-    tws-demo-ng = {
+    e-comm-ng = {
       min_size     = 2
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t2.large"]
-      capacity_type  = "SPOT"
+      instance_types = ["m7i-flex.large"]
+      capacity_type  = "ON_DEMAND"
 
-      disk_size = 35 
-      use_custom_launch_template = false  # Important to apply disk size!
+      ami_type  = "AL2023_x86_64_STANDARD"
+      disk_size = 35
+
+      labels = {
+        Environment = var.environment
+        Project     = var.project_name
+      }
 
       tags = {
-        Name = "tws-demo-ng"
-        Environment = "dev"
-        ExtraTag = "e-commerce-app"
+        Name        = "e-comm-ng"
+        Environment = var.environment
+        Project     = var.project_name
       }
     }
   }
- 
+
   tags = local.tags
-
-
 }
 
-data "aws_instances" "eks_nodes" {
-  instance_tags = {
-    "eks:cluster-name" = module.eks.cluster_name
-  }
+resource "aws_vpc_security_group_ingress_rule" "jenkins_to_eks_api" {
+  security_group_id            = module.eks.cluster_security_group_id
+  referenced_security_group_id = aws_security_group.jenkins.id
 
-  filter {
-    name   = "instance-state-name"
-    values = ["running"]
-  }
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
 
-  depends_on = [module.eks]
+  description = "Allow Jenkins EC2 to access EKS API"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "bastion_to_eks_api" {
+  security_group_id            = module.eks.cluster_security_group_id
+  referenced_security_group_id = aws_security_group.bastion.id
+
+  ip_protocol = "tcp"
+  from_port   = 443
+  to_port     = 443
+
+  description = "Allow Bastion Host to access EKS API"
 }
