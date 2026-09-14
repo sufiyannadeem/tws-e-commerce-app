8469d98276999e4484219d99ef023f1f79c6d723
Ubuntu <ubuntu@ip-10-0-1-56.eu-west-1.compute.internal>
2026-09-14 11:44:54 +0000

    Add dedicated Kubernetes healthcheck

---
+++ src/app/healthcheck/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-static";
+
+export async function GET() {
+  return NextResponse.json(
+    { status: "ok" },
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+}
